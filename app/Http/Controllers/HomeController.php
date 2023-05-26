4ebdedeb9609ffa6d497a667e628dd59a4a053ba
bennyekaa <benny.eka@um.ac.id>
2023-05-27 04:53:32 +0700

    sementara

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,12 +6,14 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use App\Models\UserBendungan;
-
+use DB;
 class HomeController extends Controller
 {
     public function index()
     {
-        return view('beranda.index');
+        // return view('beranda.index');
+        $data['bendungan'] = DB::table('ref_bendungan')->first();
+        return view('beranda.detailbendungan', $data);
     }
 
     public function tabel()
