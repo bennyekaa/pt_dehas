1643491fb8ba3fcc8c5b8f52fa9df32f47fa8bbc
DESKTOP-ULT2UGT\adity <dhion93@gmail.com>
2023-06-13 22:23:36 +0700

    ampun ndan

--- resources/views/transaksi/bocor/tambah.blade.php
+++ resources/views/transaksi/bocor/tambah.blade.php
@@ -5,13 +5,13 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Masukkan Ketinggian Muka Air</div>
+                <div class="card-header">Masukkan Status Bocor Bendungan</div>
                 <div class="card-body">
                     <form action="/transaksi/bocor/proses" method="post" enctype="multipart/form-data">
 
                         @csrf
 
-                        <div class="row mb-3">
+                        <!-- <div class="row mb-3">
                             <label for="kategori" class="col-md-4 col-form-label text-md-end">Status Bocor</label>
                             <div class="col-md-6">
                                 <select name="kategori" id="kategori" class="form-control col-md-6 select2" required>
@@ -21,16 +21,62 @@
                                     @endforeach
                                 </select>
                             </div>
-                        </div>
+                        </div> -->
 
                         <div class="row mb-3">
-                            <label for="status_bocor" class="col-md-4 col-form-label text-md-end">Keterangan Bocor</label>
+                            <label class="col-md-4 col-form-label text-md-end">Status Bocor</label>
                             <div class="col-md-6">
-                                <select name="status_bocor" id="status_bocor" class="form-control col-md-6 select2" required>
-
-                                    <option value="">--Pilih Keterangan Bocor--</option>
-
+                                <select name="pilihan" id="pilihan" class="form-control col-md-6" required>
+                                    <option value="-">-</option>
+                                    <option value="didih">DIDIH PASIR</option>
+                                    <option value="gempa">GEMPA BUMI</option>
                                 </select>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3" id="didih" style="display: none">
+                            <label class="col-md-4 col-form-label text-md-end">Lokasi</label>
+                            <div class="col-md-6">
+                                <input type="text" id="lokasi" name="lokasi" class="form-control col-md-6" required>
+                            </div>
+
+                            <label class="col-md-4 col-form-label text-md-end">Ukuran</label>
+                            <div class="col-md-6">
+                                <input type="text" id="ukuran" name="ukuran" class="form-control col-md-6" required>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="file" class="col-md-4 col-form-label text-md-end">Pilih File</label>
+                                <div class="col-md-6">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3" id="gempa" style="display: none">
+                            <label class="col-md-4 col-form-label text-md-end">Lokasi</label>
+                            <div class="col-md-6">
+                                <input type="text" id="lokasi" name="lokasi" class="form-control col-md-6" required>
+                            </div>
+
+                            <label class="col-md-4 col-form-label text-md-end">Kekuatan</label>
+                            <div class="col-md-6">
+                                <input type="text" id="kekuatan" name="kekuatan" class="form-control col-md-6" required>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="file" class="col-md-4 col-form-label text-md-end">Pilih File</label>
+                                <div class="col-md-6">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                    <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
+                                </div>
                             </div>
                         </div>
 
@@ -41,16 +87,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <label for="file" class="col-md-4 col-form-label text-md-end">Pilih File</label>
-                            <div class="col-md-6">
-                                <input type="file" class="form-control-file" id="data_file" name="data_file" accept="image/*" onchange="return fileValidation()">
-                            </div>
-                        </div>
                         <div class=" row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Simpan') }}
+                                    {{('Simpan')}}
                                 </button>
                             </div>
                         </div>
@@ -64,6 +104,22 @@
 @endsection
 
 @section('tambahanjs')
+
+<script type="text/javascript">
+    document.getElementById('pilihan').onchange = function() {
+        if (this.value == 'didih') {
+            document.getElementById('didih').style.display = 'block';
+        } else {
+            document.getElementById('didih').style.display = 'none';
+        }
+        if (this.value == 'gempa') {
+            document.getElementById('gempa').style.display = 'block';
+        } else {
+            document.getElementById('gempa').style.display = 'none';
+        }
+    };
+</script>
+
 <script type="text/javascript">
     function fileValidation() {
         var fileInput =
@@ -98,7 +154,8 @@
     $('#kategori').change(function() {
         var id = $('#kategori').val();
         console.log(id);
-        $.get('{{url('transaksi/bocor/get_status')}}/' + id,
+        $.get('{{url('
+            transaksi / bocor / get_status ')}}/' + id,
             function(data) {
                 console.log(data);
                 $('#status_bocor').empty();
@@ -110,4 +167,6 @@
             });
     });
 </script>
+
+
 @endsection
