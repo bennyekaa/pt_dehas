38a07819085a87a248fdef907b20b03a8150fefc
bennyekaa <benny.eka@um.ac.id>
2023-09-18 13:11:36 +0700

    testing

--- app/Http/Controllers/AndroidController.php
+++ app/Http/Controllers/AndroidController.php
@@ -40,7 +40,7 @@ class AndroidController extends Controller
                 $data['gambar'] = peta::find($data['trans']->id_peta);
             }
         }
-        // dd($data);
+        dd($data);
         session()->put('android_map', url()->full());
         return view('transaksi.android.peta', $data);
     }
