ae46705958d30956df895abc1e7b712c7896f56b
bennyekaa <benny.eka@um.ac.id>
2023-09-20 10:03:52 +0700

    new

--- app/Exports/LaporanExport.php
+++ app/Exports/LaporanExport.php
@@ -6,70 +6,45 @@ use Maatwebsite\Excel\Concerns\FromCollection;
 use Illuminate\Contracts\View\View;
 use Maatwebsite\Excel\Concerns\FromView;
 use Illuminate\Support\Facades\DB;
+use Maatwebsite\Excel\Concerns\WithDrawings;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
 
-
-
-class LaporanExport implements FromView
+class LaporanExport implements FromView, WithDrawings, ShouldAutoSize
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    protected $data;
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
     public function view(): View
     {
-        return view('laporan.cetak', [
-            'laporan' => DB::select("SELECT
-	notif.id_referensi,
-	notif.role_muka_air,
-	notif.role_bocor,
-	notif.`status` AS status_notif,
-	notif.aktif AS aktif_notif,
-	notif.pesan_default,
-	notif.pesan_pemda,
-	notif.pesan_umum,
-	data_banjir_bocor.id_banjir_bocor,
-	data_banjir_bocor.`status` AS status_bocor,
-	data_banjir_bocor.aktif AS aktif_bocor,
-	data_banjir_bocor.lokasi,
-	data_banjir_bocor.ukuran,
-	data_banjir_bocor.tinggi_MAW,
-	data_banjir_bocor.debit,
-	data_banjir_bocor.kekuatan,
-	data_banjir_bocor.diameter,
-	data_banjir_bocor.tinggi,
-	data_banjir_bocor.panjang,
-	data_banjir_bocor.lebar,
-	data_banjir_bocor.file_1,
-	data_banjir_bocor.file_2,
-	data_banjir_bocor.file_3,
-	data_banjir_bocor.file_4,
-	data_banjir_bocor.file_5,
-	data_banjir_bocor.created_at as created_at_bocor,
-	data_banjir_bocor.created_by as created_by_bocor,
-	data_banjir_bocor.updated_at as updated_at_bocor,
-	data_banjir_bocor.updated_by as updated_by_bocor,
-	data_banjir_bocor.id_peta as peta_bocor,
-	data_banjir_bocor.tinggi_air as tinggi_bocor,
-	data_banjir_muka_air.muka_air,
-	data_banjir_muka_air.tinggi_air as tinggi_muka_air,
-	data_banjir_muka_air.debit_air,
-	data_banjir_muka_air.id_banjir_muka_air,
-	data_banjir_muka_air.`status` as status_muka_air,
-	data_banjir_muka_air.aktif as aktif_muka_air,
-	data_banjir_muka_air.created_at as created_at_muka_air,
-	data_banjir_muka_air.created_by as created_by_muka_air,
-	data_banjir_muka_air.updated_at as updated_at_muka_air,
-	data_banjir_muka_air.updated_by as updated_by_muka_air,
-	data_banjir_muka_air.id_peta as peta_muka_air
-FROM
-	notif
-	LEFT JOIN
-	data_banjir_bocor
-	ON
-		notif.id_referensi = data_banjir_bocor.id_banjir_bocor
-	LEFT JOIN
-	data_banjir_muka_air
-	ON
-		notif.id_referensi = data_banjir_muka_air.id_banjir_muka_air WHERE notif.status <> 0")
-        ]);
+        return view('laporan.cetak', ['laporan' => $this->data]);
+    }
+
+    public function drawings()
+    {
+        $drawings = [];
+
+        // Loop through your data and add drawings for each image
+        foreach ($this->data as $item) {
+            for ($i = 1; $i <= 5; $i++) {
+                $imageColumn = 'file_' . $i;
+                if (!empty($item->$imageColumn)) {
+                    $imagePath = storage_path('app/' . $item->$imageColumn); // Adjust the path as needed
+                    $drawing = new Drawing();
+                    $drawing->setName('Dokumentasi ' . $i);
+                    $drawing->setDescription('Dokumentasi ' . $i);
+                    $drawing->setPath($imagePath);
+                    $drawing->setHeight(100); // Set the height of the image
+                    $drawing->setCoordinates('F' . $item->your_row_number); // Set the cell where the image should appear
+                    $drawings[] = $drawing;
+                }
+            }
+        }
+
+        return $drawings;
     }
 }
--- app/Http/Controllers/LaporanController.php
+++ app/Http/Controllers/LaporanController.php
@@ -7,6 +7,9 @@ use App\Exports\LaporanExport;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Facades\Excel;
+use PhpOffice\PhpSpreadsheet\Worksheet\Drawing;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class LaporanController extends Controller
 {
@@ -68,13 +71,70 @@ FROM
         return view('laporan.index', $data);
     }
 
-    public function export(){
-        return Excel::download(new LaporanExport, 'laporan.xlsx');
+    public function export()
+    {
+        $laporanData = DB::select("SELECT
+	notif.id_referensi,
+	notif.role_muka_air,
+	notif.role_bocor,
+	notif.`status` AS status_notif,
+	notif.aktif AS aktif_notif,
+	notif.pesan_default,
+	notif.pesan_pemda,
+	notif.pesan_umum,
+	data_banjir_bocor.id_banjir_bocor,
+	data_banjir_bocor.`status` AS status_bocor,
+	data_banjir_bocor.aktif AS aktif_bocor,
+	data_banjir_bocor.lokasi,
+	data_banjir_bocor.ukuran,
+	data_banjir_bocor.tinggi_MAW,
+	data_banjir_bocor.debit,
+	data_banjir_bocor.kekuatan,
+	data_banjir_bocor.diameter,
+	data_banjir_bocor.tinggi,
+	data_banjir_bocor.panjang,
+	data_banjir_bocor.lebar,
+	data_banjir_bocor.file_1,
+	data_banjir_bocor.file_2,
+	data_banjir_bocor.file_3,
+	data_banjir_bocor.file_4,
+	data_banjir_bocor.file_5,
+	data_banjir_bocor.created_at as created_at_bocor,
+	data_banjir_bocor.created_by as created_by_bocor,
+	data_banjir_bocor.updated_at as updated_at_bocor,
+	data_banjir_bocor.updated_by as updated_by_bocor,
+	data_banjir_bocor.id_peta as peta_bocor,
+	data_banjir_bocor.tinggi_air as tinggi_bocor,
+	data_banjir_muka_air.muka_air,
+	data_banjir_muka_air.tinggi_air as tinggi_muka_air,
+	data_banjir_muka_air.debit_air,
+	data_banjir_muka_air.id_banjir_muka_air,
+	data_banjir_muka_air.`status` as status_muka_air,
+	data_banjir_muka_air.aktif as aktif_muka_air,
+	data_banjir_muka_air.created_at as created_at_muka_air,
+	data_banjir_muka_air.created_by as created_by_muka_air,
+	data_banjir_muka_air.updated_at as updated_at_muka_air,
+	data_banjir_muka_air.updated_by as updated_by_muka_air,
+	data_banjir_muka_air.id_peta as peta_muka_air
+FROM
+	notif
+	LEFT JOIN
+	data_banjir_bocor
+	ON
+		notif.id_referensi = data_banjir_bocor.id_banjir_bocor
+	LEFT JOIN
+	data_banjir_muka_air
+	ON
+		notif.id_referensi = data_banjir_muka_air.id_banjir_muka_air
+WHERE notif.status <> 0");
+
+        return Excel::download(new LaporanExport($laporanData), 'laporan.xlsx');
     }
 
-    public function lihat(){
+    public function lihat()
+    {
         $data['laporan'] =
-        DB::select("SELECT
+            DB::select("SELECT
 	notif.id_referensi,
 	notif.role_muka_air,
 	notif.role_bocor,
